6c594b81cab46adbbf93c8b0f03fbb7d883d4b34
Filip Krstic <filip.krstic@gmail.com>
2018-02-21 22:29:35 +0100

    Taggable trait fixes

--- app/Traits/Taggable.php
+++ app/Traits/Taggable.php
@@ -6,34 +6,34 @@ use App\Tag;
 
 trait Taggable
 {
-    /**
-     * Fetch all tags for the model.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
-     */
-    public function tags()
-    {
-        return $this->morphToMany(Tag::class, 'taggable')->withTimestamps();
-    }
+	/**
+	 * Fetch all tags for the model.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+	 */
+	public function tags()
+	{
+		return $this->morphToMany(Tag::class, 'taggable')->withTimestamps();
+	}
 
-    /**
-     * Create tags on the fly and sync them with the model.
-     * 
-     * @param  mixed $tags
-     * @return void
-     */
-    public function tag($tags)
-    {
-    	if (isset($tags))
-    	{
-    		$ids = [];
-    		foreach ($tags as $value)
-    		{
-    			$tag = Tag::firstOrCreate(['name' => $value]);
-    			$ids[] = $tag->id;
-    		}
+	/**
+	 * Create tags on the fly and sync them with the model.
+	 * 
+	 * @param  mixed $tags
+	 * @return void
+	 */
+	public function tag($tags)
+	{
+		if (isset($tags))
+		{
+			$ids = [];
+			foreach ($tags as $value)
+			{
+				$tag = Tag::firstOrCreate(['name' => $value]);
+				$ids[] = $tag->id;
+			}
 
-    		$this->tags()->sync($ids);
-    	}
-    }
+			$this->tags()->sync($ids);
+		}
+	}
 }
