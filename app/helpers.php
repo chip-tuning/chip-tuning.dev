636191af1e4e30ad0e6f70d69d0f0cc5712df979
Filip Krstic <filip.krstic@gmail.com>
2018-02-24 18:20:30 +0100

    Multilanguage support

--- app/helpers.php
+++ app/helpers.php
@@ -13,3 +13,18 @@ if (!function_exists('set_active')) {
 		return (request()->is($pattern)) ? $value : '';
 	}
 }
+
+if (! function_exists('words')) {
+    /**
+     * Limit the number of words in a string.
+     *
+     * @param  string  $value
+     * @param  int     $words
+     * @param  string  $end
+     * @return string
+     */
+    function words($value, $words = 100, $end = '...')
+    {
+        return \Illuminate\Support\Str::words($value, $words, $end);
+    }
+}
